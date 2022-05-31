6723f4031c1112e826367895b572b18aa31ccf87
Andrey But'ko <booandrew23@gmail.com>
2022-06-01 00:51:55 +0300

    ui/theme toggle was added

--- client/icons/index.js
+++ client/icons/index.js
@@ -2,5 +2,6 @@ import { ArrowRight } from './Arrows'
 import { Tick } from './Tick'
 import { LogoutIcon } from './LogoutIcon'
 import { Chevron } from './Chevron'
-
-export { ArrowRight, Tick, LogoutIcon, Chevron }
+import { Moon } from './Moon'
+import { Sun } from './Sun'
+export { Moon, Sun, ArrowRight, Tick, LogoutIcon, Chevron }
