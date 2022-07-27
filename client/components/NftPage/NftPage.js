d8ec1f17c4593d29d955b0ecf0b64d8c39be76ed
3TERRRNITY <qertisww@gmail.com>
2022-07-27 21:59:34 +0300

    Edit NftPage.js

--- client/components/NftPage/NftPage.js
+++ client/components/NftPage/NftPage.js
@@ -4,25 +4,6 @@ const NftPage = () => {
   const { data } = useAllTokens()
 
   return (
-    // Grid
-
-    // <div className="grid grid-cols-4">
-    //   {data?.map(({ image, name }) => (
-    //     <div className="m-5 rounded-md text-black h-auto w-auto bg-white box-border">
-    //       <img src={image} alt={name} className="rounded-md" />
-    //       <div className="">
-    //         <div className="flex justify-between pt-4 font-gallery">
-    //           <h3 className="pl-3.5 font-bold leading-7">{name}</h3>
-    //           <h1 className="pr-3.5 leading-6">1.99 ETH</h1>
-    //         </div>
-    //         <p className="pl-3.5 py-5 font-normal">Account</p>
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
-
-    // FlexBox
-
     <div className="flex flex-wrap ">
       {data?.map(({ image, name }) => (
         <div className="shadow-xl relative m-5 rounded-md w-auto h-[32rem] text-black bg-white box-border font-gallery dark:bg-[#0D0A1C] dark:text-[#ECEFF1]">
