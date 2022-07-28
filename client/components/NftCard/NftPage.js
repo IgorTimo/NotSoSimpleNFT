f9ccae852e9bea091a0fa225e24281efb83d2b02
Yuri.P <domosedov@mail.ru>
2022-07-28 21:38:10 +0300

    gallery

--- client/components/NftCard/NftPage.js
+++ client/components/NftCard/NftPage.js
@@ -1,15 +1,20 @@
 import NFTCard from "./NftCard";
 import { useAllTokens } from "queries";
+import { useState } from "react";
+
+
 
 const NftPage = () => {
     const res = useAllTokens().data
-    console.log(res)
-    
-    const createNft = () => {
+    console.log("result:", res)
 
+    const [nftData, setNftData] = useState([])
 
-    }
+    // const createNft = () => {
 
+        // return cardImage
+
+ 
 return ( 
 <div className="container mx-auto">
     <div className="grid grid-cols-3 gap-4">
