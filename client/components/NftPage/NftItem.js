437f5fd3402d605e04fa01098e2cbf1dd46d3342
3TERRRNITY <qertisww@gmail.com>
2022-08-03 19:58:35 +0300

    fix issues

--- client/components/NftPage/NftItem.js
+++ client/components/NftPage/NftItem.js
@@ -17,31 +17,20 @@ const NftItem = () => {
             </div>
             <div className=" text-black bg-white box-border font-gallery rounded-r-xl dark:bg-[#0D0A1C] dark:text-[#ECEFF1] ">
               <div className="flex flex-col xl:flex-row xl:justify-between pt-4">
-                <h3 className="pl-3.5 pr-3.5 font-bold leading-7 text-sm xl:text-md 2xl:text-lg">
+                <h3 className="pl-3.5 pr-3.5 font-bold leading-7 text-xl xl:text-lg 2xl:text-md">
                   {name}
                 </h3>
-                <h1 className="pt-3.5 xl:pt-0 pr-3.5 pl-3.5 leading-6 text-sm xl:text-md 2xl:text-lg">
+                <h1 className="pt-3.5 xl:pt-0 pr-3.5 pl-3.5 leading-6 text-xl xl:text-lg 2xl:text-md">
                   1.99 ETH
                 </h1>
               </div>
-              <p className="pt-3.5 xl:py-5 pl-3.5  text-sm">Account</p>
+              <p className="pt-3.5 xl:py-5 pl-3.5  text-lg xl:text-md 2xl:text-sm">
+                Account
+              </p>
             </div>
           </div>
         </div>
       ))}
-
-      {/* <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
-    <div class="md:shrink-0">
-      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/store.jpg" alt="Man looking at item at a store">
-    </div>
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-      <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-    </div>
-  </div>
-</div> */}
     </>
   )
 }
