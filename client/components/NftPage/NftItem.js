67caa5fe141f5369b01f3c28f6a04d858b548741
3TERRRNITY <qertisww@gmail.com>
2022-08-03 19:50:39 +0300

    remade interface

--- client/components/NftPage/NftItem.js
+++ client/components/NftPage/NftItem.js
@@ -6,24 +6,42 @@ const NftItem = () => {
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       {data?.map(({ image, name }) => (
-        <div className="2xl:shadow-xl relative m-2 xl:m-3 2xl:m-5 rounded-md w-auto h-auto xl:h-[16rem] 2xl:h-[32rem] ">
-          <img
-            src={image}
-            alt={name}
-            className="w-32 xl:w-48 2xl:w-72 rounded-md"
-          />
-          {/* как сделать выезжающее меню при наведении? */}
-          <div className="hidden text-sm xl:text-md 2xl:text-lg 2xl:inline text-black bg-white box-border font-gallery dark:bg-[#0D0A1C] dark:text-[#ECEFF1]">
-            <div className="absolute bottom-0 w-full hidden md:flex flex-col content-end ">
-              <div className="flex justify-between pt-4">
-                <h3 className="pl-3.5 font-bold leading-7 ">{name}</h3>
-                <h1 className="pr-3.5 leading-6">1.99 ETH</h1>
+        <div className=" w-96 md:w-auto m-7 rounded-xl overflow-hidden">
+          <div className="flex xl:flex-col">
+            <div className="md:shrink-0">
+              <img
+                src={image}
+                alt={name}
+                className="w-72 xl:w-64 2xl:w-72 rounded-md"
+              />
+            </div>
+            <div className=" text-black bg-white box-border font-gallery rounded-r-xl dark:bg-[#0D0A1C] dark:text-[#ECEFF1] ">
+              <div className="flex flex-col xl:flex-row xl:justify-between pt-4">
+                <h3 className="pl-3.5 pr-3.5 font-bold leading-7 text-sm xl:text-md 2xl:text-lg">
+                  {name}
+                </h3>
+                <h1 className="pt-3.5 xl:pt-0 pr-3.5 pl-3.5 leading-6 text-sm xl:text-md 2xl:text-lg">
+                  1.99 ETH
+                </h1>
               </div>
-              <p className="pl-3.5 py-5">Account</p>
+              <p className="pt-3.5 xl:py-5 pl-3.5  text-sm">Account</p>
             </div>
           </div>
         </div>
       ))}
+
+      {/* <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/store.jpg" alt="Man looking at item at a store">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div> */}
     </>
   )
 }
