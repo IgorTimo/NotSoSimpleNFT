39688c4d30abcf4d24d8dc1026a817c4c1a1e589
Alexpn <alexpvn69@gmail.com>
2022-09-09 00:08:54 +0300

    корректировки 08_09_22

--- client/components/NftPage/NFTListContainer.js
+++ client/components/NftPage/NFTListContainer.js
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { useAllTokens } from '../../queries'
 import Modal from '../Modal/Modal'
 import NFTItem from './NFTItem'
-import NFTsList from './NFTsList'
+import { useWeb3Context } from '../../hooks/useWeb3Context'
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 
 const NFTListContainer = () => {
   const [modalActive, setModalActive] = useState(false)
   const { data } = useAllTokens()
-const [selectedItem, setSelectedItem] = useState(undefined)
+  const [selectedItem, setSelectedItem] = useState(undefined)
+  const { account } = useWeb3Context()
 
   const dataWithId = data?.map(el => {
     return {...el,
@@ -16,20 +18,33 @@ const [selectedItem, setSelectedItem] = useState(undefined)
 const handleNftItemClick = (id) => {
  const focusedItem = dataWithId.find(el => el.id === id ); 
  setSelectedItem(focusedItem)
+ setModalActive(true)
 }
   return (
     <>
       <div
         className="flex flex-wrap w-full"
-        onClick={() => setModalActive(true)}
+  
       >
         {dataWithId?.map(({ image, name, id }) => (
            <NFTItem image={image} name={name} id={id} handleNftItemClick={handleNftItemClick}/>
          
         ))}
       </div>
-      <div>
-        <Modal active={modalActive} setActive={setModalActive}>{selectedItem?.description}</Modal>
+      <div className='text-center'>
+        <Modal active={modalActive} setActive={setModalActive}>
+         <span className='text-lg font-bold'>{selectedItem?.name}</span> <br />
+         <div className="mr-2 text-md font-bold">Owner:
+         <Jazzicon diameter={18} seed={jsNumberForAddress(account)} />
+         {account.toString().slice(0,5)+'...'+account.toString().slice(38)}
+                  </div><br />
+                  <img
+            src={selectedItem?.image}
+            className="w-52 h-34 rounded-md"
+          />
+          {/* xl:w-34 2xl:w-42 */}
+         {selectedItem?.description}
+        </Modal>
       </div>
     </>
   )
